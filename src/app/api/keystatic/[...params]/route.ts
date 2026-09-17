a6ae7b2ac5b970219ca67111eb9e26552c652f1e
budctx <bud.ctx1@gmail.com>
2026-09-17 22:54:25 +0300

    Allow Keystatic build before GitHub App credentials are configured

--- src/app/api/keystatic/[...params]/route.ts
+++ src/app/api/keystatic/[...params]/route.ts
@@ -1,4 +1,9 @@
 import {makeRouteHandler} from '@keystatic/next/route-handler';
 import config from '../../../../../keystatic.config';
 
-export const {POST, GET} = makeRouteHandler({config});
+export const {POST, GET} = makeRouteHandler({
+  clientId: process.env.KEYSTATIC_GITHUB_CLIENT_ID || 'placeholder',
+  clientSecret: process.env.KEYSTATIC_GITHUB_CLIENT_SECRET || 'placeholder',
+  secret: process.env.KEYSTATIC_SECRET || 'placeholder',
+  config,
+});
