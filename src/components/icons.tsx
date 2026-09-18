a9191f67184fb66804fba19340a222a0a5494491
budctx <bud.ctx1@gmail.com>
2026-09-18 22:58:38 +0300

    revert: remove copy email icon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -34,15 +34,6 @@ export function MailIcon(props: PortfolioIconProps) {
   );
 }
 
-export function CopyIcon(props: PortfolioIconProps) {
-  return (
-    <svg {...common} {...props}>
-      <rect x="8" y="8" width="12" height="12" rx="2" />
-      <path d="M16 8V6a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2" />
-    </svg>
-  );
-}
-
 export function LanguagesIcon(props: PortfolioIconProps) {
   return (
     <svg {...common} {...props}>
