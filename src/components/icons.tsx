01fcd4ecd1e3a05f2fde5763de882382f182dccb
budctx <bud.ctx1@gmail.com>
2026-09-19 13:10:07 +0300

    feat: add contact detail icons

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -34,6 +34,31 @@ export function MailIcon(props: PortfolioIconProps) {
   );
 }
 
+export function MessageCircleIcon(props: PortfolioIconProps) {
+  return (
+    <svg {...common} {...props}>
+      <path d="M21 15a4 4 0 0 1-4 4H8l-5 3V7a4 4 0 0 1 4-4h10a4 4 0 0 1 4 4z" />
+    </svg>
+  );
+}
+
+export function PhoneIcon(props: PortfolioIconProps) {
+  return (
+    <svg {...common} {...props}>
+      <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6A19.79 19.79 0 0 1 2.12 4.18 2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72c.12.9.33 1.78.62 2.63a2 2 0 0 1-.45 2.11L8 9.73a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45c.85.29 1.73.5 2.63.62A2 2 0 0 1 22 16.92z" />
+    </svg>
+  );
+}
+
+export function MapPinIcon(props: PortfolioIconProps) {
+  return (
+    <svg {...common} {...props}>
+      <path d="M20 10c0 5-8 12-8 12S4 15 4 10a8 8 0 1 1 16 0Z" />
+      <circle cx="12" cy="10" r="3" />
+    </svg>
+  );
+}
+
 export function LanguagesIcon(props: PortfolioIconProps) {
   return (
     <svg {...common} {...props}>
